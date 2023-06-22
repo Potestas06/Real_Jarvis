7c541d2936026c5efd0656d79c773ba75dde6268
Potestas06 <Patrick@krispel.com>
2023-06-22 11:21:00 +0200

    made it kinda work but not realy

--- test.py
+++ test.py
@@ -1,40 +1,147 @@
-import pvporcupine
-import pyaudio
 import os
+import requests
 from dotenv import load_dotenv
-import struct
+import openai
+import json
+import functions
 
 load_dotenv()
+openai.api_key = os.getenv("OPENAIKEY")
+url = "https://api.openai.com/v1/chat/completions"
+previous = []
+function_list = [
+        {
+            "name": "check_weather",
+            "description": "Get the current weather from a city.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "city": {
+                        "type": "string",
+                        "description": "The city to get the weather from, e.g. London"
+                    }
+                },
+                "required": ["city"]
+            }
+        },
+        {
+            "name": "create_task",
+            "description": "Creates a task",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "content": {
+                        "type": "string",
+                        "description": "The content of the task, e.g. buy milk"
+                    }
+                },
+                "required": ["content"]
+            }
+        },
+        {
+            "name": "check_task_status",
+            "description": "Checks if a task is done",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "task_name": {
+                        "type": "string",
+                        "description": "The name of the task, e.g. buy milk"
+                    }
+                },
+                "required": ["task_name"]
+            }
+        },
+        {
+            "name": "close_task_by_name",
+            "description": "Closes a task with a given name",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "task_name": {
+                        "type": "string",
+                        "description": "The name of the task, e.g. buy milk"
+                    }
+                },
+                "required": ["task_name"]
+            }
+        },
+        {
+            "name": "get_undone_tasks",
+            "description": "Gets all undone tasks and returns them as a list",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "task_name": {
+                        "type": "string",
+                        "description": "A list of all undone tasks"
+                    }
+                }
+            },
+            "required": []
+        }
+    ]
 
-APIKEY = os.getenv("APIKEY")
+def second_request(function_name, content):
+    message = [{
+        "role": "function",
+        "name": function_name,
+        "content": content
+    }]
+    messages = message + [{"role": msg["role"], "content": msg["content"]} for msg in previous]
 
-# Load the wake word model
-handle = pvporcupine.create(access_key=APIKEY, keywords=['computer'])
+    response = openai.ChatCompletion.create(
+        model=os.getenv("MODEL"),
+        messages=messages,
+        functions=function_list,
+        function_call="auto"
+    )
 
-# Initialize PyAudio
-audio = pyaudio.PyAudio()
+    print("response: " + response)
+    message = response.choices[0].message # type: ignore
+    previous.append(message)
+    return message["content"]
 
-# Set the audio stream parameters
-stream = audio.open(
-    rate=handle.sample_rate,
-    channels=1,
-    format=pyaudio.paInt16,
-    input=True,
-    frames_per_buffer=handle.frame_length
-)
+def request(text):
+    message = [{
+        "role": "user",
+        "content": text
+    }]
+    messages = message + [{"role": msg["role"], "content": msg["content"]} for msg in previous]
 
-print("Listening for wake word...")
+    response = openai.ChatCompletion.create(
+        model=os.getenv("MODEL"),
+        messages=messages,
+        functions=function_list,
+        function_call="auto"
+    )
+    print(response)
+    message = response.choices[0].message # type: ignore
+    previous.append(message)
 
-while True:
-    pcm = stream.read(handle.frame_length)
-    pcm = struct.unpack_from("h" * handle.frame_length, pcm)
+    if "function_call" in message:
+        function_call = message["function_call"]
+        arguments = json.loads(function_call["arguments"])
 
-    keyword_index = handle.process(pcm)
+        response = None
+        function_name = function_call["name"]
 
-    if keyword_index >= 0:
-        print("Wake word detected!")
-        # Perform action or trigger further processing based on the wake word detection
+        if function_name == "check_weather":
+            print(arguments["city"])
+            response = functions.check_weather(arguments["city"])
+        elif function_name == "create_task":
+            response = functions.create_task(arguments["content"])
+        elif function_name == "check_task_status":
+            response = functions.check_task_status(arguments["task_name"])
+        elif function_name == "close_task_by_name":
+            response = functions.close_task_by_name(arguments["task_name"])
+        elif function_name == "get_undone_tasks":
+            response = functions.get_undone_tasks()
 
-# Clean up resources
-stream.close()
-audio.terminate()
+        if response is not None:
+            print(response)
+            second_request(function_name, response)
+    else:
+        return message["content"]
+
+request("what is the wehater in berlin?")
